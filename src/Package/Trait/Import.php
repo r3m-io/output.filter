34e6680dbad4858ae68a0d08af20ef6b52939dd2
remco van der Velde <remco.pc@outlook.com>
2023-12-23 15:48:12 +0100

    output filter

---
+++ src/Package/Trait/Import.php
@@ -0,0 +1,23 @@
+<?php
+namespace Package\R3m\Io\OutputFilter\Trait;
+
+use R3m\Io\App;
+
+use R3m\Io\Module\Core;
+use R3m\Io\Module\File;
+
+use R3m\Io\Node\Model\Node;
+
+use Exception;
+trait Import {
+
+    public function role_system(): void
+    {
+        $object = $this->object();
+        $package = $object->request('package');
+        if($package){
+            $node = new Node($object);
+            $node->role_system_create($package);
+        }
+    }
+}
--- src/Package/View/Setup/Setup.tpl
+++ src/Package/View/Setup/Setup.tpl
@@ -1,4 +1,4 @@
-### Setup
-
-
-
+{{$register = Package.R3m.Io.OutputFilter:Init:register()}}
+{{if(!is.empty($register))}}
+{{Package.R3m.Io.OutputFilter:Import:role.system()}}
+{{/if}}
