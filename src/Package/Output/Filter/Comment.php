5e71772fcef488cc6139ba77de397b789af41594
remco van der Velde <remco.pc@outlook.com>
2024-07-21 00:48:17 +0200

    comment remove

---
+++ src/Package/Output/Filter/Comment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Package\R3m\Io\Output\Filter\Output\Filter;
+
+use R3m\Io\App;
+
+use R3m\Io\Module\Controller;
+
+class Comment extends Controller
+{
+
+    public static function remove(App $object, $response = null): mixed
+        ddd($response);
+        return $response;
+    }
+
+}
