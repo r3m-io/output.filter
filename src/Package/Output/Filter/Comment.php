8899f4a7ddbae5913a34d4bbd372854b8f18d907
remco van der Velde <remco.pc@outlook.com>
2024-07-21 01:14:10 +0200

    comment remove

--- src/Package/Output/Filter/Comment.php
+++ src/Package/Output/Filter/Comment.php
@@ -5,15 +5,16 @@ namespace Package\R3m\Io\Output\Filter\Output\Filter;
 use R3m\Io\App;
 
 use R3m\Io\Module\Controller;
+use R3m\Io\Module\Parse\Value;
 
 class Comment extends Controller
 {
 
     public static function remove(App $object, $destination, $filter, $options = []): mixed
     {
-        d($destination);
-        d($filter);
-        ddd($options);
+        if(array_key_exists('response', $options)){
+            $options['response'] = Value::remove_comment($options['response']);
+        }
         return $options['response'];
     }
 
